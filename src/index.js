d9841ce92c6d1bfe4279cfc66174e7619e1d026a
irada-garayeva <irada.garayeva98@gmail.com>
2023-09-14 00:37:04 +0200

    Improving reusability with props

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      className="test"
+      defaultRating={3}
+    />
   </React.StrictMode>
 );
